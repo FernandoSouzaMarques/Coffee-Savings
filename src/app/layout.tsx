23b50a802cf055c9435fc825de1c61b8c5a3d438
Fernando S. Marques <fernando.souza@codebit.com.br>
2024-09-11 14:37:33 -0300

    feat: configure recoil context on project

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import RecoilContextProvider from "@/app/recoilContextProvider";
 import "@/styles/main.css";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -16,7 +17,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <RecoilContextProvider>{children}</RecoilContextProvider>
+      </body>
     </html>
   );
 }
