fffe7aff0822d7e3fd41bb90e4caaabd146d9455
Fernando S. Marques <fernando.souza@codebit.com.br>
2024-09-11 14:39:12 -0300

    chore: refactor client fetcher for use revalidate tags

--- src/config/client.ts
+++ src/config/client.ts
@@ -2,12 +2,19 @@ type TMethod = "GET" | "POST" | "PUT" | "PATCH" | "DELETE";
 
 interface IParams {
   method: TMethod;
+	body?: BodyInit | null;
+	next?: NextFetchRequestConfig;
 }
 
 const baseURL = process.env.NEXT_PUBLIC_API_URL;
 export const client = async (route: string, params?: IParams) => {
   return await fetch(`${baseURL}${route}`, {
-		method: params?.method ?? "GET"
+		headers: {
+			'Content-Type': 'application/json',
+		},
+		method: params?.method ?? "GET",
+		body: params?.body ?? undefined,
+		next: params?.next,
 	})
 	.then((resp) => resp.json())
 	.catch((err) => console.error(err));
