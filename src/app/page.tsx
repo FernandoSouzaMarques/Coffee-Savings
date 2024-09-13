ed795f4a9baf4bc55de25ea9402e0e964448660c
Fernando S. Marques <fernando.souza@codebit.com.br>
2024-09-13 14:03:44 -0300

    feat: update first message when does not has profiles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -20,19 +20,27 @@ async function getUsers(): Promise<IUserProfile[]> {
 export default async function Home() {
   const profiles = await getUsers();
 
+  const hasProfiles = profiles.length > 0;
+
   return (
     <main className="flex flex-col items-center justify-center min-h-screen relative">
-      <h1 className="text-3xl text-center font-bold">Who will access it?</h1>
+      <h1 className="text-3xl text-center font-bold">
+        {hasProfiles
+          ? "Who will access it?"
+          : "Before you start, create a profile"}
+      </h1>
 
       <ProfileList list={profiles} />
       <AddProfileButton />
 
-      <Link
-        href={PagesEnum.MANAGE_PROFILE}
-        className="uppercase font-bold absolute bottom-10 left-1/2 -translate-x-1/2"
-      >
-        Manage profiles
-      </Link>
+      {hasProfiles && (
+        <Link
+          href={PagesEnum.MANAGE_PROFILE}
+          className="uppercase font-bold absolute bottom-10 left-1/2 -translate-x-1/2"
+        >
+          Manage profiles
+        </Link>
+      )}
 
       <AddProfileModal />
     </main>
