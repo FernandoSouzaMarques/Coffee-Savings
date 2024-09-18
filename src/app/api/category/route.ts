954ee81bffbeddb85d20936060aafdd5430c50f4
Fernando S. Marques <fernando.souza@codebit.com.br>
2024-09-18 13:09:30 -0300

    feat: update category GET endpoint for includes subCategories

--- src/app/api/category/route.ts
+++ src/app/api/category/route.ts
@@ -2,9 +2,29 @@ import { NextRequest, NextResponse } from "next/server";
 import { client, trx } from "@/lib/connect";
 import { revalidateTag } from "next/cache";
 
+interface ISubCategory {
+  id: string,
+  name: string,
+}
+
+interface ICategory {
+  id: string,
+  name: string,
+  isExpense: boolean,
+  subCategories: ISubCategory[]
+}
+
+interface IQueryCategory {
+  id: string,
+  name: string,
+  isExpense: boolean,
+  subCategoryId: string,
+  subCategoryName: string,
+}
 
 export async function GET(request: NextRequest) {
-  const rawQuery = 'SELECT id, name, "isExpense" FROM public."Category"';
+  const rawQuery =
+    'SELECT c.id, c.name, c."isExpense", s.id as "subCategoryId", s.name as "subCategoryName" FROM public."Category" c LEFT JOIN public."SubCategory" s ON c.id = s."categoryId"';
   const url = new URL(request.url);
   const categoryId = url.searchParams.get("id");
 
@@ -12,7 +32,35 @@ export async function GET(request: NextRequest) {
     const response = await client.query(
       `${rawQuery} ${categoryId ? `WHERE id = ${categoryId}` : ""}`
     );
-    return NextResponse.json(!!categoryId ? response.rows[0] : response.rows);
+
+    const results: IQueryCategory[] = !!categoryId ? response.rows[0] : response.rows;
+
+    const formatted = results.reduce<ICategory[]>((acc, curr) => {
+      let category = acc.find((cat) => cat.id === curr.id);
+
+      if (!category) {
+        category = {
+          id: curr.id,
+          name: curr.name,
+          isExpense: curr.isExpense,
+          subCategories: [],
+        };
+        acc.push(category);
+      }
+
+      console.log(curr)
+
+      if (curr.subCategoryId) {
+        category.subCategories.push({
+          id: curr.subCategoryId,
+          name: curr.subCategoryName,
+        });
+      }
+
+      return acc;
+    }, []);
+
+    return NextResponse.json(formatted);
   } catch (err) {
     return NextResponse.json(
       { error: "Error retrieving category data", details: err },
@@ -25,14 +73,15 @@ export async function POST(request: NextRequest) {
   const body = JSON.parse(await request.text());
   const tag = request.nextUrl.searchParams.get("tag");
   if (tag) revalidateTag(tag);
-  
+
   try {
     await trx.query("BEGIN");
-    const queryText = 'INSERT INTO public."Category"(name, icon, "isExpense") VALUES ($1, $2, $3)';
+    const queryText =
+      'INSERT INTO public."Category"(name, icon, "isExpense") VALUES ($1, $2, $3)';
     const values = [body.name, body.icon, body.isExpense];
-    
+
     await trx.query(queryText, values);
-    
+
     await trx.query("COMMIT");
     return NextResponse.json({ status: 201 });
   } catch (err) {
