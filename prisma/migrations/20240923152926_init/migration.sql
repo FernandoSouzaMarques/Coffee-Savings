cdb16d4b6e62b80b1fefa249e082ff001e0ae6dc
Fernando S. Marques <fernando.souza@codebit.com.br>
2024-09-23 12:55:56 -0300

    feat: removes currentInvoice that table credit card

---
+++ prisma/migrations/20240923152926_init/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `currentInvoice` on the `CreditCard` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "CreditCard" DROP COLUMN "currentInvoice";
